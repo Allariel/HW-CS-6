93d38d9544d5baaf16ecaa1c50014dab483960bb
Vitaliy Prilutskiy <Hamachigame@yandex.ru>
2022-10-20 10:11:54 +0700

    Initial Commit

---
+++ Zadacha-41/Program.cs
@@ -0,0 +1,28 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+(int, string)Count_Positive_Num(int sum, string check)
+{
+    try
+    {
+        int number = Convert.ToInt32(check);
+        if (number>0)
+        {
+            sum++;
+        }
+    }
+    catch (FormatException)
+    {
+      check = "M";
+    }
+    return (sum,check);
+}
+
+int sum = 0;
+string check = "O";
+Console.WriteLine("Пиши числа или М чтобы остановить");
+while (check != "M")
+{
+    check = Console.ReadLine();
+   (sum,check)=Count_Positive_Num(sum,check);
+}
+Console.WriteLine("Чисел >0: " + sum + "\n");
---
+++ Zadacha-43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// k1*x+b1=k2*x+b2
+// k1*x-k2*x = b2-b1
+// x = b2-b1/k1-k2 
+void XY(double [] k, double [] b)
+{
+    if(k[0]==k[1])
+    {
+        Console.WriteLine("Прямые параллельны");
+        if(b[0]==b[1])
+        {
+             Console.WriteLine("и совпадают");
+        }
+        return;
+    }
+    double x = (b[1] - b[0]) / (k[0] - k[1]);
+    double y = k[0] * x + b[0];
+    Console.WriteLine("("+ x + ";" + y + ")" );
+}
+
+Console.WriteLine("Привет, напиши k1 b1 k2 b2!");
+double[] k = new double[2];
+double[] b = new double[2];
+
+for (int i = 0; i < 2; i++)
+{
+    k[i]= Convert.ToDouble(Console.ReadLine());
+    b[i]= Convert.ToDouble(Console.ReadLine());
+}
+XY(k,b);
+
+
+
